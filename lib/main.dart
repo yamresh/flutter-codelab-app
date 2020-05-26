c9567f7642c24754e4856435a279502fb8d513a0
Amresh Yadav <amresh.yadav@publicissapient.com>
2020-05-27 00:13:29 +0530

    list the saved fiels

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext conttext) {
     return MaterialApp(
       title: "Welcome to Flutters",
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('Welcome to Flutter'),
-          ),
           body: Center(
-            child: RandomWords(),
-          )),
+        child: RandomWords(),
+      )),
     );
   }
 }
@@ -25,6 +25,37 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+            (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return Scaffold(
+            // Add 6 lines from here...
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          ); // ... to here.
+        },
+      ),
+    );
+  }
+
   @override // Add fr@override
   Widget build(BuildContext context) {
     //final wordPair = WordPair.random(); // Delete these...
@@ -34,12 +65,20 @@ class RandomWordsState extends State<RandomWords> {
       // Add from here...
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.list),
+            onPressed: _pushSaved,
+          )
+        ],
       ),
       body: _buildSuggestions(),
     ); // ... to here.
   }
 
   final List<WordPair> _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>();
+
   final TextStyle _biggerFont = TextStyle(fontSize: 10);
 
   Widget _buildSuggestions() {
@@ -76,11 +115,25 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 }
